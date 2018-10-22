8cf51b0c0db0f2bd165e3e2d5a48c5c01ce9295e
David Truxall <david.truxall@perspecta.com>
2018-10-22 17:11:26 -0400

    Functional list animation

--- app/src/main/java/com/truxall/everydayanimation/ui/ListFragment.kt
+++ app/src/main/java/com/truxall/everydayanimation/ui/ListFragment.kt
@@ -9,9 +9,16 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 
 import com.truxall.everydayanimation.R
 import com.truxall.everydayanimation.data.Album
+import com.truxall.everydayanimation.R.id.album_view
+import android.view.animation.LayoutAnimationController
+
+
+
+
 
 class ListFragment : Fragment() {
 
@@ -32,15 +39,31 @@ class ListFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
 
-        val recyclerView =  this.containingView!!.findViewById<RecyclerView>(R.id.recyclerview)
+        val recyclerView =  this.containingView!!.findViewById<RecyclerView>(R.id.album_view)
         val adapter = AlbumListAdapter(this.requireContext())
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this.context)
 
+        // Apply animation to the list
+        val resId = R.anim.layout_animation_fall_down
+        val animation = AnimationUtils.loadLayoutAnimation(this.context, resId)
+        recyclerView.layoutAnimation = animation
+
         val albumObserver = Observer<List<Album>?> { newList ->
             adapter.setWords(newList)
+            // Run the animation on update
+            // this.runLayoutAnimation(recyclerView)
         }
         viewModel!!.artistAlbums.observe(this, albumObserver)
+    }
+
+    private fun runLayoutAnimation(recyclerView: RecyclerView) {
+        val context = recyclerView.context
+        val controller = AnimationUtils.loadLayoutAnimation(context, R.anim.layout_animation_fall_down)
+
+        recyclerView.layoutAnimation = controller
+        recyclerView.adapter!!.notifyDataSetChanged()
+        recyclerView.scheduleLayoutAnimation()
     }
 
 }
