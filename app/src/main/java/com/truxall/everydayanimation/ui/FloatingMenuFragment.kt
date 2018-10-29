8846ae445137f5568e28945bbe8a748f96b5621f
David Truxall <2066163+davetrux@users.noreply.github.com>
2018-10-28 22:07:57 -0400

    Functional floating menu, no animation

--- app/src/main/java/com/truxall/everydayanimation/ui/FloatingMenuFragment.kt
+++ app/src/main/java/com/truxall/everydayanimation/ui/FloatingMenuFragment.kt
@@ -1,11 +1,16 @@
 package com.truxall.everydayanimation.ui
 
+import android.opengl.Visibility
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 import com.truxall.everydayanimation.R
 
@@ -16,16 +21,57 @@ class FloatingMenuFragment : Fragment() {
     }
 
     private lateinit var viewModel: FloatingMenuViewModel
+    private var menuOpen: Boolean = false
+    private lateinit var fab: FloatingActionButton
+    private lateinit var createButton: FloatingActionButton
+    private lateinit var shareButton: FloatingActionButton
+    private lateinit var shareLayout: LinearLayout
+    private lateinit var addLayout: LinearLayout
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.floating_menu_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(FloatingMenuViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        this.fab = view.findViewById(R.id.mainFab)
+        this.shareButton = view.findViewById(R.id.shareFab)
+        this.createButton = view.findViewById(R.id.createFab)
+
+        this.shareLayout = view.findViewById(R.id.shareLayout)
+        this.addLayout = view.findViewById(R.id.createLayout)
+
+        this.fab.setOnClickListener {
+            if(this.menuOpen) {
+                this.closeMenu()
+                this.menuOpen = false
+            } else {
+                this.openMenu()
+                this.menuOpen = true
+            }
+        }
+
+        this.shareButton.setOnClickListener {
+            this.menuClicked(view)
+        }
+        this.createButton.setOnClickListener {
+            this.menuClicked(view)
+        }
     }
 
+    private fun menuClicked(view: View) {
+        Toast.makeText(view.context, "Tapped", Toast.LENGTH_SHORT).show()
+    }
+    private fun openMenu() {
+        this.shareLayout.visibility = View.VISIBLE
+        this.addLayout.visibility = View.VISIBLE
+    }
+
+    private fun closeMenu() {
+        this.shareLayout.visibility = View.INVISIBLE
+        this.addLayout.visibility = View.INVISIBLE
+    }
 }
