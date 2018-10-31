d1d6254cf538e8ecd030c5d9b46731b00f9f749d
David Truxall <david.truxall@perspecta.com>
2018-10-31 15:38:03 -0400

    Added shaking button

--- app/src/main/java/com/truxall/everydayanimation/ui/ButtonAttentionFragment.kt
+++ app/src/main/java/com/truxall/everydayanimation/ui/ButtonAttentionFragment.kt
@@ -5,23 +5,33 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.truxall.everydayanimation.R
-import com.truxall.everydayanimation.ui.controls.ProgressButton
+import android.view.animation.AnimationUtils
+
 
 class ButtonAttentionFragment: Fragment() {
-    
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.button_attention_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val fab = view.findViewById<FloatingActionButton>(R.id.attention_fab)
+        val shake = AnimationUtils.loadAnimation(context, R.anim.shake)
 
+        val runnable = {
+            fab.startAnimation(shake)
+        }
+
+        val handler = Handler()
+        handler.postDelayed(runnable, 1500)
+        handler.postDelayed(runnable, 3000)
+        handler.postDelayed(runnable, 4500)
+        handler.postDelayed(runnable, 6000)
     }
 }
