d07796166b9a2eaa2e689b077583305e39db28a8
David Truxall <david.truxall@perspecta.com>
2018-11-05 08:20:20 -0500

    Cleanup and minor changes

--- app/src/main/java/com/truxall/everydayanimation/ui/ButtonAttentionFragment.kt
+++ app/src/main/java/com/truxall/everydayanimation/ui/ButtonAttentionFragment.kt
@@ -5,6 +5,7 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.truxall.everydayanimation.R
@@ -12,6 +13,9 @@ import android.view.animation.AnimationUtils
 
 
 class ButtonAttentionFragment: Fragment() {
+
+    private var shakeRepeats: Int = 0
+    private val totalShakes: Int = 2
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -29,9 +33,22 @@ class ButtonAttentionFragment: Fragment() {
         }
 
         val handler = Handler()
+
+        shake.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+               if (shakeRepeats < totalShakes) {
+                   handler.postDelayed(runnable, 1500)
+                   shakeRepeats++
+               }
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+            }
+        })
+
         handler.postDelayed(runnable, 1500)
-        handler.postDelayed(runnable, 3000)
-        handler.postDelayed(runnable, 4500)
-        handler.postDelayed(runnable, 6000)
     }
 }
