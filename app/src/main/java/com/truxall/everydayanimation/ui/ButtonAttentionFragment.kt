5e4d1eb5fb674ba285ed83ff75cdf7430187a449
David Truxall <david.truxall@perspecta.com>
2018-12-12 17:06:28 -0500

    Migrated the view animation to a property animation

--- app/src/main/java/com/truxall/everydayanimation/ui/ButtonAttentionFragment.kt
+++ app/src/main/java/com/truxall/everydayanimation/ui/ButtonAttentionFragment.kt
@@ -1,21 +1,20 @@
 package com.truxall.everydayanimation.ui
 
+import android.animation.*
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.truxall.everydayanimation.R
-import android.view.animation.AnimationUtils
 
 
 class ButtonAttentionFragment: Fragment() {
 
     private var shakeRepeats: Int = 0
-    private val totalShakes: Int = 2
+    private val totalShakes: Int = 3
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -25,30 +24,52 @@ class ButtonAttentionFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Find the view we want to animate
         val fab = view.findViewById<FloatingActionButton>(R.id.attention_fab)
-        val shake = AnimationUtils.loadAnimation(context, R.anim.shake)
+        // Remember the starting position
+        val x = fab.x
 
-        val runnable = {
-            fab.startAnimation(shake)
-        }
+        // Create animations to move the button left to right and rotate a little
+        val translate = ObjectAnimator.ofFloat(fab, "translationX", -5f, 5f)
+        translate.repeatCount = 10
+        translate.repeatMode = ValueAnimator.REVERSE
+
+        val rotate = ObjectAnimator.ofFloat(fab, "rotation", -10f, 10f)
+        rotate.repeatCount = 10
+        rotate.repeatMode = ValueAnimator.REVERSE
+
+        // Run the two animations together as a set
+        val animSetXY = AnimatorSet()
+        animSetXY.duration = 70
+        animSetXY.playTogether(rotate, translate)
 
         val handler = Handler()
+        val runnable = {
+            animSetXY.start()
+        }
 
-        shake.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationRepeat(animation: Animation?) {
+        // When the rotation is done, reset the view, track the number of runs, and run again
+        rotate.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationCancel(animation: Animator?) {
             }
 
-            override fun onAnimationEnd(animation: Animation?) {
-               if (shakeRepeats < totalShakes) {
-                   handler.postDelayed(runnable, 1500)
-                   shakeRepeats++
-               }
+            override fun onAnimationStart(animation: Animator?) {
             }
 
-            override fun onAnimationStart(animation: Animation?) {
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                fab.x = x
+                fab.rotation = 0f
+                if (shakeRepeats < totalShakes) {
+                    handler.postDelayed(runnable, 2000)
+                    shakeRepeats++
+                }
             }
         })
 
-        handler.postDelayed(runnable, 1500)
+        // Start the handler
+        handler.postDelayed(runnable, 2000)
     }
 }
